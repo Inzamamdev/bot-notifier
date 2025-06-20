cff5b708d6f4c8c4f2028e82f3a59e4d62ee87b0
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-06-20 21:46:17 +0530

    Implement API endpoints and web-based login

--- apis/views.py
+++ apis/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Item
 from .serializers import ItemSerializer
 
 class PublicView(APIView):
+    permission_classes = [AllowAny]
     def get(self, request):
         items = Item.objects.all()
         serializer = ItemSerializer(items, many=True)
--- bot_notifier/urls.py
+++ bot_notifier/urls.py
@@ -20,7 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apis/', include('apis.urls')),
+    path('api/', include('apis.urls')),
     path('', include('userauth.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
